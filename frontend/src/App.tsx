61b23aff053fbb34a864f1346df94f15010cefc8
Abdullah <abdullah_b04@outlook.com>
2025-09-01 19:07:32 +0300

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">App</div>
+    <div className="text-3xl font-bold underline">
+      Hello world!
+      <p className="mt-4 text-lg">
+        This is a simple example of a React component styled with Tailwind CSS.
+      </p>
+    </div>
   )
 }
 
